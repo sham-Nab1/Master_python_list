6ab0efc37cbdffada2bc4110ee72c7f2c4afcd75
Sham-Nab1 <shamfrancisco06@gmail.com>
2024-09-20 21:28:58 +0800

    social_media

---
+++ social_media_list.py
@@ -0,0 +1,31 @@
+# social_media_list.py
+
+# List of 10 social media platforms.
+social_media_platforms= [
+    "WeChat", "Pinterest", "LinkedIn", "Tumblr", "YouTube"
+    "Facebook", "Twitter", "Reddit", "Snapchat", "TikTok",
+]
+
+# Print the entire list.
+print("Social Media Platforms:")
+print(social_media_platforms)
+
+# Access and print the 5th index.
+print()
+print("5th index is:", social_media_platforms[4])
+
+# Update the 3rd index to "Instagram".
+social_media_platforms[2] = "Instagram"
+print()
+print("Updated list after changing 3rd index to 'Instagram':")
+print(social_media_platforms)
+
+# Delete the 8th index.
+del social_media_platforms[7]
+print()
+print("Updated list after deleting the 8th index:")
+print(social_media_platforms)
+
+# Print the last index of the list.
+print()
+print("Last index is:", social_media_platforms[-1])
